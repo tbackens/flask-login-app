f334faecb6aff6d4c60a784309f45480864b5f4a
tbackens <99706186+tbackens@users.noreply.github.com>
2023-02-04 11:49:59 +0100

    some changes

--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired])
-    password = PasswordField('username', validators=[DataRequired])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('username', validators=[DataRequired()])
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired])
-    password = PasswordField('password', validators=[DataRequired, EqualTo('password_confirmation', message='Passwords must match')])
-    password_confirmation = PasswordField('repeat_password', validators=[DataRequired])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), EqualTo('password_confirmation', message='Passwords must match')])
+    password_confirmation = PasswordField('repeat_password', validators=[DataRequired()])
--- models.py
+++ models.py
@@ -6,14 +6,14 @@ from werkzeug.security import check_password_hash, generate_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
 
     def create_pw_hash(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_pw_hash(self, password):
-        return check_password_hash(password)
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self):
-        return self.username
+        return '<User {}>'.format(self.username)
