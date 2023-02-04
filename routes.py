fdb1d94821dd34045979c0817262ea7833cafef4
tbackens <99706186+tbackens@users.noreply.github.com>
2023-02-04 11:49:31 +0100

    added signup route

--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
 from app import app, db
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from forms import RegisterForm, LoginForm
+from models import User
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 
@@ -10,7 +13,27 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@app.route('signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated():
+        flash('You are already SignedIn')
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.create_pw_hash(form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
+        flash('Successfully registered')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+        
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
