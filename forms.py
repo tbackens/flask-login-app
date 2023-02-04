50af379d18605c25e04fc2bec7a12c3bcf4cfc7e
tbackens <99706186+tbackens@users.noreply.github.com>
2023-02-04 11:18:50 +0100

    added forms

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired])
+    password = PasswordField('username', validators=[DataRequired])
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired])
+    password = PasswordField('password', validators=[DataRequired, EqualTo('password_confirmation', message='Passwords must match')])
+    password_confirmation = PasswordField('repeat_password', validators=[DataRequired])
