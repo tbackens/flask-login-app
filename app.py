67ae998b98775db6cd559b912fdb53972a79fe18
tbackens <99706186+tbackens@users.noreply.github.com>
2023-02-04 11:57:33 +0100

    added login_manager

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'my_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 
 
 
-#login_manager = LoginManager()
-#login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 
