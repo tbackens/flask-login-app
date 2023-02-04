631979a928abcd66eeabd28e9f9012a1471f6630
tbackens <99706186+tbackens@users.noreply.github.com>
2023-02-04 10:36:28 +0100

    added user model

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def create_pw_hash(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_pw_hash(self, password):
+        return check_password_hash(password)
+
+
+    def __repr__(self):
+        return self.username
