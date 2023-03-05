61cc9d803ad39ec0e3308dac313cc3ee67fca750
tbackens <99706186+tbackens@users.noreply.github.com>
2023-03-06 01:54:08 +0530

    added route for login page

--- routes.py
+++ routes.py
@@ -16,7 +16,7 @@ def index():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
-        flash('You are already SignedIn')
+        flash('You are already LoggedIn')
         return redirect(url_for('index'))
     form = RegisterForm()
     if form.validate_on_submit():
@@ -31,9 +31,21 @@ def signup():
     return render_template('signup.html', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        flash('You are already LoggedIn')
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_pw_hash(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
         
 
 with app.app_context():
